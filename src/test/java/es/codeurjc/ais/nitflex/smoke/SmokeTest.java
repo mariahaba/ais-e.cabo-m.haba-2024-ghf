788933305d77db3c9b23e96f63c1a87816a48299
elisacabog <129116256+elisacabog@users.noreply.github.com>
2024-05-11 15:15:25 +0200

    Update SmokeTest.java

--- src/test/java/es/codeurjc/ais/nitflex/smoke/SmokeTest.java
+++ src/test/java/es/codeurjc/ais/nitflex/smoke/SmokeTest.java
@@ -1,47 +1,48 @@
-package es.codeurjc.ais.nitflex.smoke;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.time.Duration;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.openqa.selenium.chrome.ChromeOptions;
-import es.codeurjc.ais.nitflex.Application;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.time.Duration;
-
-@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SmokeTest {
-	
-    @LocalServerPort
-    int port;
 
     private WebDriver driver;
-    private WebDriverWait wait;
 
     @BeforeEach
-    public void setup() {
+    public void setUp() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
-        this.driver = new ChromeDriver(options);
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); // Espera implícita de 10 segundos
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 
     @Test
     public void testWelcomeMessage() {
         String host = System.getProperty("host");
         if (host == null) {
-            throw new IllegalStateException("Host URL must be provided.");
+            fail("Host URL must be provided.");
         }
-        host = host + this.port + "/";
+        host += this.port + "/";
+        
         driver.get(host);
-        String welcomeMessage = driver.findElement(By.className("ui header")).getText();
+        
+        WebElement header = driver.findElement(By.className("ui header"));
+        String welcomeMessage = header.getText();
         assertEquals("Welcome to Nitflex", welcomeMessage, "Welcome message is not correct");
     }
 }
