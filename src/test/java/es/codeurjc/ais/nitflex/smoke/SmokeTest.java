2538c51e8ff5ea69a72cf097d55f04b3568aaad0
mariahaba <129094226+mariahaba@users.noreply.github.com>
2024-05-13 12:54:00 +0200

    Update SmokeTest.java

--- src/test/java/es/codeurjc/ais/nitflex/smoke/SmokeTest.java
+++ src/test/java/es/codeurjc/ais/nitflex/smoke/SmokeTest.java
@@ -6,39 +6,42 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.openqa.selenium.chrome.ChromeOptions;
+import es.codeurjc.ais.nitflex.Application;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+import java.time.Duration;
+
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SmokeTest {
 	
+    @LocalServerPort
+    int port;
+
     private WebDriver driver;
+    private WebDriverWait wait;
 
     @BeforeEach
     public void setup() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
         this.driver = new ChromeDriver(options);
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(2));
     }
 
     @Test
     public void testWelcomeMessage() {
-        String host = System.getProperty("host"); // Asegúrate de que esta es la URL completa
+        String host = System.getProperty("host");
         if (host == null) {
             throw new IllegalStateException("Host URL must be provided.");
         }
+        host = host + this.port + "/";
         driver.get(host);
         String welcomeMessage = driver.findElement(By.className("ui header")).getText();
         assertEquals("Welcome to Nitflex", welcomeMessage, "Welcome message is not correct");
     }
-
-    @AfterEach
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
 }
-
